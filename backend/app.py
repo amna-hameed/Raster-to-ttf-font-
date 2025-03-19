3a0b24ba814dd14c27ca1387ee048248374fe460
Amna Hameed <133674716+amna-hameed@users.noreply.github.com>
2025-03-19 23:46:16 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -221,5 +221,5 @@ def upload_image():
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
